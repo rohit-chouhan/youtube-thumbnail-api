b19e8fc86925f7cfdae82a78d0e814bc04fd7f4c
Rohit Chouhan <34239087+rohit-chouhan@users.noreply.github.com>
2020-09-10 11:53:36 -0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,23 @@
+{      
+ "name": "youtube-thumbnail-api",                                    
+ "version": "1.0.0",                                                    
+ "description": "Open Source Javascript Framework to Get all size and quality thumbnail of Youtube Video from URL",                             
+ "main": "api,js",                                                
+ "scripts": {                                                             
+   "test": "echo \"Error: no test specified\" && exit 1"              
+ },                                                                         
+ "keywords": [                                                            
+  "youtube","api","javascript","js","framework"                                                                 
+ ],                                                                         
+ "author": "Rohit Chouhan",                                             
+ "license": "MIT",                                                      
+ "repository": {                                                         
+    "type": "git",                                                     
+     "url": "git+https://github.com/rohit-chouhan/youtube-thumbnail-api.git"          
+ },                                                                         
+ "bugs": {                                                               
+    "url": "https://github.com/rohit-chouhan/youtube-thumbnail-api/issues"           
+ },                                                                        
+  "homepage": "https://github.com/rohit-chouhan/youtube-thumbnail-api#readme",      
+  "_publishConfig": { "registry": "https://npm.pkg.github.com/" }    
+  }
